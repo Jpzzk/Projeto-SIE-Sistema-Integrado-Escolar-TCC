9b2f7f56275830e410c6cfa0bd977e2058fa50a9
Jpzzk <jp.ambrosio007@gmail.com>
2026-05-27 11:20:54 -0300

    login atualizado

--- login.js
+++ login.js
@@ -1,16 +1,53 @@
-function fazerLogin() {
-    const usuario = document.getElementById('usuario').value;
-    const senha = document.getElementById('senha').value;
-  
-    // Login fixo (simples)
-    if (usuario === 'admin' && senha === '1234') {
-      localStorage.setItem('logado', 'true');
-      window.location.href = 'index.html';
-    } else {
-      alert('Usuário ou senha incorretos');
-    }
+// LOGIN
+
+function login() {
+
+  const usuario =
+    document.getElementById('usuario').value;
+
+  const senha =
+    document.getElementById('senha').value;
+
+  const erro =
+    document.getElementById('erroLogin');
+
+  // LOGIN PADRÃO
+
+  if (
+    usuario === 'joao' &&
+    senha === '1234'
+  ) {
+
+    localStorage.setItem('logado', 'true');
+
+    localStorage.setItem('usuario', usuario);
+
+    window.location.href = 'index.html';
+
+  } else {
+
+    erro.innerText =
+      'Usuário ou senha incorretos';
+
   }
-  function logout() {
-    localStorage.removeItem('logado');
-    window.location.href = 'login.html';
+
+}
+
+// MOSTRAR SENHA
+
+function mostrarSenha() {
+
+  const senha =
+    document.getElementById('senha');
+
+  if (senha.type === 'password') {
+
+    senha.type = 'text';
+
+  } else {
+
+    senha.type = 'password';
+
   }
+
+}
